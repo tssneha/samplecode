543a4887dcaecc14c640317d33383359d8b06263
tssneha <snehasiri999@gmail.com>
2023-05-01 16:21:16 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,7 +9,7 @@ function Footer() {
         <div className="footer-content">
           <div className="footer-section about">
             <p className="footer-para">
-              Developer: <span className="my-name"> Tadawvkugwviywrvuvwbi</span>
+              Developer: <span className="my-name"> ABC</span>
             </p>
 
             <div className="contact"></div>
