7775519347c93ace2a4f64b73304c14d534d4cd9
tssneha <snehasiri999@gmail.com>
2023-05-01 16:12:40 +0530

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,18 +7,18 @@ function About() {
         <div className="about-nike">
           <h2 className="about-nike-text">OUR MISSION</h2>
           <p>
-            "BRING INSPIRATION AND INNOVATION TO EVERY ATHLETE* IN THE WORLD"
+            "BRINGING INSPIRATION AND INNOVATION TO EVERY ATHLETE IN THE WORLD"
           </p>
         </div>
       </div>
       <div className="about-nike-part">
-        <h2 className="about-nike-heading">Abouddddt Nidddke</h2>
+        <h2 className="about-nike-heading">About Nike</h2>
         <p className='about-nike-para'>
-          Our missioddn is what drives us to do everything possible to expand
-          human potential. We do that by creating groundbreadddking sport
-          innovations, by making our products more sudddstainably, by building a
-          creative and diverseacadcadc global team and by making a positive impact in
-          communities where sacascawe live and work. Based in Beaverton, Oregon, NIKE,
+          Our mission is what drives us to do everything possible to expand
+          human potential. We do that by creating ground breaking sport
+          innovations, by making our products more suitable, by building a
+          creative and diverse global team and by making a positive impact in
+          communities where we live and work. Based in Beaverton, Oregon, NIKE,
           Inc. incluascacvfvz des the Nike, Converse, and Jordan brands.
         </p>
       </div>
